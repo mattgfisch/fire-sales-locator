2ed6d810fdc237b11dc8834ace12fca30618f840
Jordan Kamin <jordanakamin@gmail.com>
2017-03-27 15:56:10 -0700

    moves articles out of root

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
-get '/' do
+get '/articles' do
   @articles = Article.all
   erb :'articles/index'
+end
+
+get '/articles/:id' do # '/articles/:id'
+  @article = Article.find(params[:id])
+  erb :'articles/show'
 end
