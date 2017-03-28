4c99ca2d7f68480056b20c0362a878a63fab3836
Jordan Kamin <jordanakamin@gmail.com>
2017-03-28 14:12:57 -0700

    finishes mornign breakout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,3 +7,19 @@ get '/articles/:id' do # '/articles/:id'
   @article = Article.find(params[:id])
   erb :'articles/show'
 end
+
+get '/articles/:id/edit' do
+  @article = Article.find(params[:id])
+  erb :'articles/edit'
+end
+
+put '/articles/:id' do
+  @article = Article.find(params[:id])
+  @article.title = params[:title]
+  @article.body = params[:body]
+  @article.save
+  erb :'articles/show'
+end
+
+post '/articles' do
+end
