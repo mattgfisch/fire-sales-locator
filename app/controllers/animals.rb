2d157f264e9f5497372ee960f6e159d51ae8a299
Jordan Kamin <jordanakamin@gmail.com>
2017-04-06 10:01:45 -0700

    build new/create pages/routes

--- app/controllers/animals.rb
+++ app/controllers/animals.rb
@@ -2,3 +2,18 @@ get '/animals' do
   @animals = Animal.all
   erb :'animals/index'
 end
+
+get '/animals/new' do
+  # @animal = Animal.new
+  erb :'animals/new'
+end
+
+get '/animals/:id' do
+  @animal = Animal.find(params[:id])
+  erb :'animals/show'
+end
+
+post '/animals' do
+  new_animal = Animal.create(params[:animal])
+  redirect to "/animals/#{new_animal.id}"
+end
